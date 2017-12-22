728f502a6fc9d078a49825f8753936f089e3cf63
taq-f <mysecondproject@gmail.com>
2017-12-22 12:42:57 +0900

    Prepare Status component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StatusComponent } from './subcomponents/status.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/subcomponents/status.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.component.html',
+  styleUrls: ['./status.component.css']
+})
+export class StatusComponent {
+}
