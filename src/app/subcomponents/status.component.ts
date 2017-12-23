c97fd1354ade25e0de0ed47b231f2f2703611032
taq-f <mysecondproject@gmail.com>
2017-12-23 14:15:31 +0900

    Status component has eventemitter of selected entry change.
    Each file change entry needs to have staged or unstaged stage because showing diff will be implemented later on.

--- src/app/subcomponents/status.component.ts
+++ src/app/subcomponents/status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { AppStatusEntry, AppWorkingFileChange } from '../models/workingfile';
 import { StatusService } from '../services/status.service';
@@ -10,12 +10,32 @@ import { StatusService } from '../services/status.service';
 })
 export class StatusComponent implements OnInit {
 
-  stagedChanges: ReadonlyArray<AppWorkingFileChange>;
+  /**
+   * Changes entries that will be placed in unstaged changes.
+   */
   unstagedChanges: ReadonlyArray<AppWorkingFileChange>;
+  /**
+   * Changes entries that will be placed in staged changes.
+   */
+  stagedChanges: ReadonlyArray<AppWorkingFileChange>;
+  /**
+   * Changes entries that will be placed in conflicted changes.
+   */
   conflictedChanges: ReadonlyArray<AppWorkingFileChange>;
 
+  /**
+   * A currently selected change entry.
+   */
   selectedChange: AppWorkingFileChange;
 
+  /**
+   * Event fires when selected change entry changed.
+   */
+  @Output() entrySelectChange = new EventEmitter<AppWorkingFileChange>();
+
+  /**
+   * This component requires StatusService to retrieve file change entries.
+   */
   constructor(private statusService: StatusService) { }
 
   ngOnInit(): void {
@@ -26,7 +46,11 @@ export class StatusComponent implements OnInit {
     });
   }
 
+  /**
+   * Select specified change entry.
+   */
   selectChange(c: AppWorkingFileChange) {
     this.selectedChange = c;
+    this.entrySelectChange.emit(c);
   }
 }
