95aab24b088097aa24eaa9d1aef0ae122ab6ce4e
taq-f <mysecondproject@gmail.com>
2017-12-24 18:22:50 +0900

    feature/repository-add brach merged.
    I want to use Angular Material from now, so merge the branch.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
+import { RepositoryAddComponent } from './subcomponents/repository-add.component';
 import { StatusComponent } from './subcomponents/status.component';
 import { ChangeEntryComponent } from './subcomponents/change-entry.component';
 
@@ -12,13 +17,21 @@ import { StatusService } from './services/status.service';
 @NgModule({
   declarations: [
     AppComponent,
+    RepositoryAddComponent,
     StatusComponent,
     ChangeEntryComponent
+  ],
+  entryComponents: [
+    RepositoryAddComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [StatusService],
   bootstrap: [AppComponent]
