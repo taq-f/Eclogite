157edefe46f7c3d597d449b1de5b6201e7568a00
taq-f <mysecondproject@gmail.com>
2017-12-27 18:09:18 +0900

    Open repository select dialog implmented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { RepositorySelectComponent } from './subcomponents/repository-select.component';
 import { AppWorkingFileChange } from './models/workingfile';
 
 @Component({
@@ -10,7 +13,17 @@ export class AppComponent {
   repositoryPath = '';
   filepath: string;
 
+  constructor(public dialog: MatDialog) { }
+
   onEntrySelectChange(v: AppWorkingFileChange): void {
     this.filepath = v.path;
+  }
+
+  selectRepository(): void {
+    console.log("select");
+    const dialogRef = this.dialog.open(RepositorySelectComponent)
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
+    });
   }
 }
