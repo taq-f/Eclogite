e5372876b01b8bb3094fab088681646ad37a2250
taq-f <mysecondproject@gmail.com>
2018-01-10 16:41:55 +0900

    Asar unpackdir specification moved to javascript API.

--- script/package.js
+++ script/package.js
@@ -13,7 +13,9 @@ const options = {
   dir: path.join(__dirname, "..", "dist"),
   appVersion: config.version,
   arch: 'x64',
-  asar: false, // don't asar pack in order to exclude git executables.
+  asar: {
+    unpackDir: "node_modules/dugite/git", // Git executables need to be outside of asar.
+  },
   electronVersion: electronVersion(config.devDependencies.electron),
   icon: path.join(__dirname, "..", "resources", "app.ico"),
   name: config.name,
