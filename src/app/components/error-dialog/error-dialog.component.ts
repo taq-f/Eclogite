9df3809de529d6d057e6b3b328d126c899830652
taq-f <mysecondproject@gmail.com>
2018-05-10 18:55:43 +0900

    Change .styl to .scss of error dialog component.

--- src/app/components/error-dialog/error-dialog.component.ts
+++ src/app/components/error-dialog/error-dialog.component.ts
@@ -4,7 +4,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 @Component({
   selector: 'app-error-dialog',
   templateUrl: './error-dialog.component.html',
-  styleUrls: ['./error-dialog.component.styl'],
+  styleUrls: ['./error-dialog.component.scss'],
 })
 export class ErrorDialogComponent {
   text: string;
